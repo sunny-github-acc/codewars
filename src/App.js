d8ea9fc7b05c966463b1fb65052b41f387920851
sunny <karolis.kazak@gmail.com>
2021-02-23 15:05:55 +0200

    middle permutations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Directions from "./unlocked/directions  reduction";
 import Double from "./unlocked/double cola";
 import Greed from "./unlocked/greed";
+import Permutations from "./unlocked/permutations";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greed />
+        <Permutations />
       </header>
     </div>
   );
